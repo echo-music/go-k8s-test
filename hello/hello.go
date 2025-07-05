b61d1d4957595ef8cba8b19d9da117168c212623
echo-music <php_fangting@126.com>
2025-07-05 22:03:03 +0800

    up

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/echo-music/go-k8s-test/order/client"
+
 	"log"
 	"net/http"
 	"os"
@@ -10,8 +11,8 @@ import (
 
 func say(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
-	client.CreateOrder()
-	_, err := fmt.Fprintln(w, hostname+" 热烈欢迎 "+r.RemoteAddr+" 到访")
+	res := client.CreateOrder()
+	_, err := fmt.Fprintln(w, hostname+" 热烈欢迎 "+r.RemoteAddr+" 到访. grpc result"+res)
 	if err != nil {
 		return
 	}
